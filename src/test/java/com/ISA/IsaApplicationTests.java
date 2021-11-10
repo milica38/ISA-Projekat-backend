bf7e6c623616a364dff8ecead6b27095f9e0281c
Milica Urukalo <74359178+milica38@users.noreply.github.com>
2021-11-10 08:07:15 +0100

    init commit

---
+++ src/test/java/com/ISA/IsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ISA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
