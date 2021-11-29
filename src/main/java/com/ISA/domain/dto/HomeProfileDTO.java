cdcf57ad66483a50e69d5fa33008abf700d431af
Milica Urukalo <74359178+milica38@users.noreply.github.com>
2021-11-29 20:47:29 +0100

    Home profile model

---
+++ src/main/java/com/ISA/domain/dto/HomeProfileDTO.java
@@ -0,0 +1,107 @@
+package com.ISA.domain.dto;
+
+import java.util.Date;
+
+public class HomeProfileDTO {
+
+    private Long id;
+    private String name;
+    private String address;
+    private String promoDescription;
+    private int numberOfRooms;
+    private int numberOfBeds;
+    private Date freeTerms;
+    private String behaviourRules;
+    private double pricelist;
+    private String extraService;
+    private String interiorImage;
+    private String exteriorImage;
+
+    private double latitude;
+    private double longitude;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public double getLatitude(){
+        return latitude;
+    }
+    public void setLatitude(double latitude){
+        this.latitude = latitude;
+    }
+    public double getLongitude(){
+        return longitude;
+    }
+    public void setLongitude(double longitude){
+        this.longitude = longitude;
+    }
+    public String getPromoDescription(){
+        return promoDescription;
+    }
+    public void setPromoDescription(String promoDescription){
+        this.promoDescription = promoDescription;
+    }
+    public Date getFreeTerms(){
+        return freeTerms;
+    }
+    public void setFreeTerms(Date freeTerms){
+        this.freeTerms = freeTerms;
+    }
+    public String getBehaviourRules(){
+        return behaviourRules;
+    }
+    public void setBehaviourRules(String behaviourRules){
+        this.behaviourRules = behaviourRules;
+    }
+    public double getPricelist(){
+        return pricelist;
+    }
+    public void setPriceList(double pricelist){
+        this.pricelist = pricelist;
+    }
+    public String getExtraService(){
+        return extraService;
+    }
+    public void setExtraService(String extraService){
+        this.extraService = extraService;
+    }
+    public int getNumberOfRooms(){
+        return numberOfRooms;
+    }
+    public void setNumberOfRooms(int numberOfRooms){
+        this.numberOfRooms = numberOfRooms;
+    }
+    public int getNumberOfBeds(){
+        return numberOfBeds;
+    }
+    public void setNumberOfBeds(int numberOfBeds){
+        this.numberOfBeds = numberOfBeds;
+    }
+    public String getInteriorImage(){
+        return interiorImage;
+    }
+    public void setInteriorImage(String interiorImage){
+        this.interiorImage = interiorImage;
+    }
+    public String getExteriorImage(){
+        return exteriorImage;
+    }
+    public void setExteriorImage(String exteriorImage){
+        this.exteriorImage = exteriorImage;
+    }
+}
