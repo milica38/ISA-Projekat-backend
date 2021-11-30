c0de47c07ed5fab255382c9494b697205dff2971
Milica Urukalo <74359178+milica38@users.noreply.github.com>
2021-11-30 01:30:29 +0100

    Boat profile controller

---
+++ src/main/java/com/ISA/controller/BoatProfileController.java
@@ -0,0 +1,55 @@
+package com.ISA.controller;
+
+import com.ISA.domain.dto.BoatProfileDTO;
+import com.ISA.domain.model.BoatProfile;
+import com.ISA.service.definition.BoatProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/boats")
+public class BoatProfileController {
+
+    @Autowired
+    private BoatProfileService boatProfileService;
+
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        List<BoatProfile> boatProfiles = boatProfileService.getAll();
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<?> get(@PathVariable Long id) {
+        BoatProfile bp = boatProfileService.get(id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> add(@RequestBody BoatProfileDTO dto) {
+        BoatProfile bp = boatProfileService.add(dto);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<?> edit(@PathVariable Long id, @RequestBody BoatProfileDTO dto) {
+        BoatProfile bp = boatProfileService.edit(dto);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        boolean delete = boatProfileService.delete(id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
