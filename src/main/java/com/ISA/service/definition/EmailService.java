1d0f3b061f1b97a68bdbc97bde7e9c8d97dd18d5
Milica Urukalo <74359178+milica38@users.noreply.github.com>
2022-01-10 17:59:23 +0100

    Action for boats and email users for actions

--- src/main/java/com/ISA/service/definition/EmailService.java
+++ src/main/java/com/ISA/service/definition/EmailService.java
@@ -1,6 +1,10 @@
 package com.ISA.service.definition;
 
 import com.ISA.domain.model.*;
+import com.ISA.domain.model.BoatProfile;
+import com.ISA.domain.model.HomeProfile;
+import com.ISA.domain.model.User;
+
 
 public interface EmailService {
      void sendEmailForRegistration(User user);
@@ -8,5 +12,6 @@ public interface EmailService {
      void sendEmailForBoatReservation(User user, BoatReservation reservation);
      void sendEmailForAdventureReservation(User user, AdventureReservation reservation);
      void sendEmailForHouseAction(User user, HomeProfile homeProfile);
+     void sendEmailForBoatAction(User user, BoatProfile boatProfile);
 
 }
--- src/main/java/com/ISA/service/implementation/EmailServiceImpl.java
+++ src/main/java/com/ISA/service/implementation/EmailServiceImpl.java
@@ -1,6 +1,9 @@
 package com.ISA.service.implementation;
 
 import com.ISA.domain.model.*;
+import com.ISA.domain.model.BoatProfile;
+import com.ISA.domain.model.HomeProfile;
+import com.ISA.domain.model.User;
 import com.ISA.service.definition.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -269,6 +272,57 @@ public class EmailServiceImpl implements EmailService {
 
             // Now set the actual message
             message.setText("Dear " + user.getName() +  ",\n\nWe have a new action for" + homeProfile.getName());
+
+            // Send message
+            Transport.send(message);
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+
+    public void sendEmailForBoatAction(User user, BoatProfile boatProfile)
+    {
+        // Sender's email ID needs to be mentioned
+        String from = "PatientServicePSWFirma1@gmail.com";
+
+        // Assuming you are sending email from localhost
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.host", host);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.socketFactory.fallback", "false");
+
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("PatientServicePSWFirma1@gmail.com","PSW!1234");
+                    }
+                });
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+
+            // Set Subject: header field
+            message.setSubject("New action for boat");
+
+            // Now set the actual message
+            message.setText("Dear " + user.getName() +  ",\n\nWe have a new action for" + boatProfile.getName());
 
             // Send message
             Transport.send(message);
